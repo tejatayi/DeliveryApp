79080d284393dd6b25e0a77b09621f6929781160
tejatayi <saisivatejatayi@gmail.com>
2025-05-18 10:08:45 -0400

    replace env with config/firebaseconfig.js

--- pages/FirebaseAuth.js
+++ pages/FirebaseAuth.js
@@ -8,19 +8,9 @@ import {
 import { useState } from "react";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios"; // this lib is used to make api calls.
+import { firebaseConfig } from "../config/firebaseConfig";
 
 //const [user_authorized, setuser_authorized] = useState(false);
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
 
 // Initialize Firebase if not already initialized
 export const signUp = (email, password, navigation) => {
